1a341860e99c7e1fd1b32a5140c09b9c00125ddd
a21angelmb <email>
2023-02-13 18:30:12 +0100

    Initial commit.

--- src/main/java/com/example/practica3modulosfx/HelloController.java
+++ src/main/java/com/example/practica3modulosfx/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.practica3modulosfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField nombre;
+    @FXML
+    private TextField apellidos;
+    @FXML
+    private TextField NIF;
+    @FXML
+    private TextField codigoPostal;
+    @FXML
+    private TextField correoElectronico;
+    @FXML
+    private Button mostrarMatricula;
+    @FXML
+    private Label horasSeleccionadas;
+    @FXML
+    private CheckBox checkBoxProgramacion;
+    @FXML
+    private CheckBox checkBoxDiseñoInterfaces;
+    @FXML
+    private CheckBox checkBoxAccesoDatos;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onMostrarMatriculaClick() {
+        //welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
